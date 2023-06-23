beddb7ac629249b8b264e6904a1bc5236417bdcf
yonip97 <70665101+yonip97@users.noreply.github.com>
2023-06-23 09:47:05 +0300

    beginning simple models

--- utils.py
+++ utils.py
@@ -23,3 +23,51 @@ def check_correlation(a, b):
     p = pearsonr(a,b)[0]
     k = kendalltau(a,b)[0]
     return p,k
+def isNaN(num):
+    return num != num
+
+
+def create_data(path):
+    onlyfiles = [join(path, f) for f in listdir(path) if (isfile(join(path, f)) and 'xlsx' in f)]
+    files_series = [pd.read_excel(f, index_col=0).reset_index(drop=True) for f in onlyfiles]
+    annotation_data = merge_annotation_results(files_series)
+    annotation_data = annotation_data.replace('None', np.NAN)
+    annotation_data = annotation_data.dropna(thresh=3)
+    annotators_names = list(annotation_data.columns)
+    annotation_data = annotation_data.astype(float)
+    relevant_attributes = {'upvote_ratio': [], 'score': [], 'num_comments': [], 'selftext': [], 'title': []}
+    with open("data/raw/raw_output_updated.pickle", 'rb') as f:
+        full_raw_data = pickle.load(f)
+    full_raw_data = [x['data'] for x in full_raw_data]
+    count = 0
+    for k in full_raw_data[0].keys():
+        attriubte = []
+        for i in range(len(full_raw_data)):
+            if k in full_raw_data[i]:
+                attriubte.append(full_raw_data[i][k])
+        try:
+            x = Counter(attriubte)
+            if x.most_common(1)[0][1] > 0.9 * len(attriubte):
+                count += 1
+            else:
+                print(k)
+            # else:
+            #     print(k)
+        except:
+            pass
+            # print("Cant hash see later")
+            # print(k)
+    print(count)
+    for sample in full_raw_data:
+        for key in relevant_attributes:
+            relevant_attributes[key].append(sample[key])
+    x = pd.DataFrame.from_dict(relevant_attributes)
+    x['example_id'] = x.index
+    full_data = annotation_data.join(x, on='example_id').reset_index(drop=True)
+    full_data = full_data.rename({'score': 'ups'}, axis=1)
+    full_data['total_votes'] = full_data['ups'] / full_data['upvote_ratio']
+    full_data['total_votes'] = full_data['total_votes'].astype(int)
+    full_data['downs'] = full_data['total_votes'] - full_data['ups']
+    # full_data = annotation_data.join(full_data_no_annotation,on='example_id')
+    return full_data, annotators_names
+
